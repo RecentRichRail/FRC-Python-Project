83a8c4ae456f3c9d7a610c6d1ec2bf04899a0073
Glen-Gorski <glen.gorski@gmail.com>
2023-07-06 14:52:07 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    message = request.json.get('message')
-    encrypted_message = encrypt(message)  # Implement your encryption logic here
-    return jsonify({'encrypted_message': encrypted_message}), 200
-
-@app.route('/receive_message', methods=['GET'])
+@app.route('/message', methods=['POST'])
 def receive_message():
-    encrypted_message = request.args.get('encrypted_message')
-    decrypted_message = decrypt(encrypted_message)  # Implement your decryption logic here
-    return jsonify({'decrypted_message': decrypted_message}), 200
-
-def encrypt(message):
-    # Implement your encryption logic here
-    return message[::-1]
-
-def decrypt(encrypted_message):
-    # Implement your decryption logic here
-    return encrypted_message[::-1]
+    message = request.form.get('message')
+    # Handle the received message, perform encryption, etc.
+    # You can store the messages in a database, relay them to other clients, etc.
+    return 'Message received'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
